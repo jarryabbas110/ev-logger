696583435a5127acbc7b51e7c957e4409665b088
unknown <jarry.abbas@outlook.com>
2022-06-27 00:51:52 +0500

    second commit

--- app.js
+++ app.js
@@ -25,4 +25,7 @@ mongoose.connect(process.env.DB_CONNECTION, () =>
     console.log('connected to db')
 );
 //how do we start listening to the servier
-app.listen(3000);
+//app.listen(3000);
+
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
